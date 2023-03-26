f27b2c8bc3ad29c5afd75ee60bb0794a33fee2bf
jaison10 <jaisonj1010@gmail.com>
2023-03-26 21:32:14 +0530

    trying to store entire DOM

--- content-script.js
+++ content-script.js
@@ -23,15 +23,12 @@
 //which key has been pressed:
 // if (event.key == 'ArrowUp') { }
 
-//cart
-let SelectedContent = [];
-
 let keysPressed = {
     Alt : false,
     Coloring : false
 };
 
-function StoreInLocal(SelectedDOM){
+function StoreInLocal(SelectedDOM, EntireDOM){
     //store it in localstorage
     console.log("SelectText ClassName ", SelectedDOM.commonAncestorContainer.className);
     console.log("SelectText ID ", SelectedDOM.commonAncestorContainer.id);
@@ -39,6 +36,7 @@ function StoreInLocal(SelectedDOM){
     console.log("SelectedText ", SelectedDOM.commonAncestorContainer.innerText);
     console.log("ParentElem ClassName ", SelectedDOM.commonAncestorContainer.parentElement.className);
     console.log("ParentElem ID ", SelectedDOM.commonAncestorContainer.parentElement.id);
+    console.log("Entire DOM is ",  EntireDOM);
 
     let CurrentContent = { 
         SelectTextClassName : SelectedDOM.commonAncestorContainer.className,
@@ -47,15 +45,12 @@ function StoreInLocal(SelectedDOM){
         SelectedText : SelectedDOM.commonAncestorContainer.innerText,
         ParentElemClassName : SelectedDOM.commonAncestorContainer.parentElement.className,
         ParentElemID : SelectedDOM.commonAncestorContainer.parentElement.id,
+        DOM: EntireDOM,
     };
     var alreadySelectedTxt = JSON.parse( localStorage.getItem("HightlightInfo"));
     alreadySelectedTxt.push(CurrentContent);
+    // console.log("Converted JSON ",  JSON.stringify(alreadySelectedTxt));
     localStorage.setItem("HightlightInfo", JSON.stringify(alreadySelectedTxt));
-
-    // localStorage.setItem("HightlightInfo", JSON.stringify(CurrentContent));
-    // SelectedContent = [...SelectedContent, CurrentContent];
-    // localStorage.setItem("HightlightInfo", JSON.stringify(SelectedContent));
-
 };
 
 function StoreAndColor(selectedText){
@@ -79,7 +74,7 @@ function StoreAndColor(selectedText){
             // Set design mode to off
             document.designMode = "off";
             //storing in local storage
-            StoreInLocal(range);
+            StoreInLocal(range,selectedText);
             //remove selected default selection
             selectedText.removeAllRanges();
         }
@@ -100,6 +95,9 @@ document.addEventListener('keydown', (event) => {
         StoreAndColor(document.getSelection());
         console.log("coloriing");
         keysPressed.Coloring = true
+   }
+   else{
+    keysPressed.Coloring = false
    }
 },);
 
@@ -132,24 +130,32 @@ document.addEventListener('keydown', (event) => {
 
 window.addEventListener("load", (event) => {
     console.log("loaaaaded");
-    var selectedText = selectedText = JSON.parse(localStorage.getItem("HightlightInfo"));
+    var selectedText = JSON.parse(localStorage.getItem("HightlightInfo"));
     console.log(selectedText);
     //create ranges.
     selectedText.forEach((eachTxt) => {
         console.log(eachTxt);
         console.log(typeof(eachTxt));
         console.log(eachTxt.SelectedText);
-        const test = eachTxt.SelectedText
+        var test = eachTxt.SelectedText
         // var range = document.createRange();
         // range.selectNode(test);
         // selection.addRange(range);
         if (test !== undefined){
-            let text = document.getElementById(eachTxt.ParentElemID).innerHTML;
-            console.log("Found Text ", text);
-            let re = new RegExp(test,"g"); // search for all instances
-		    let newText = text.replace(re, `<mark>${test}</mark>`);
-            console.log("NEW TEXT ", newText);
-		    document.getElementById(eachTxt.ParentElemID).innerHTML = newText;
+            let node = document.getElementById(eachTxt.ParentElemID);
+            console.log("Found Text ", node);
+            // let re = new RegExp(test,"g"); // search for all instances
+		    // let newText = node.replace(re, `<mark>${test}</mark>`);
+            // console.log("NEW TEXT ", newText);
+		    // document.getElementById(eachTxt.ParentElemID).innerHTML = newText;
+
+            // node.setAttribute(
+            //     "style",
+            //     "background-color: yellow; display: inline;"
+            //  );
+            //  test.surroundContents(node);
+
+            console.log("Entire DOM ", eachTxt.DOM);
         }
     });
 
